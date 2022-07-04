5a6023d53a5926dd1c31a5f570f6706309e40162
Oscar <osgrouch@proton.me>
2022-07-04 18:15:29 -0400

    Adding isValid method to Nonet

--- src/main/java/sudoku/puzzle/Nonet.java
+++ src/main/java/sudoku/puzzle/Nonet.java
@@ -1,5 +1,8 @@
 package sudoku.puzzle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Class representing a 3x3 region of cells in a Sudoku puzzle. Keeps track of the cells within the 3x3 region
  * and can return the Cells belonging to a particular row or column within this region.
@@ -39,5 +42,27 @@ public class Nonet {
 	 */
 	public Cell[] getCol (int col) {
 		return cells.getCol(col);
+	}
+
+	/**
+	 * Check that the Nonet is valid by checking that each Cell has no repeating digits
+	 * and all are in the range [1, 9].
+	 *
+	 * @return true if Nonet is valid
+	 */
+	public boolean isValid () {
+		boolean result = true;
+		ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+		for (int row = 0; row < rows; row++) {
+			Cell[] cellRow = getRow(row);
+			for (int col = 0; col < cols; col++) {
+				// remove will return true if the Integer object was found in the ArrayList of Integers
+				// meaning if the method returns false, the Cell was not set, or it repeated a digit
+				result = numbers.remove(Integer.valueOf(cellRow[col].getDigit()));
+				if (!result) break;
+			}
+			if (!result) break;
+		}
+		return result;
 	}
 }
