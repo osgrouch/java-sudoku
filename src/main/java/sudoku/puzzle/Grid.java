4fd602d3f57112b7140efd0d46c0a30ff3fca4de
Oscar <osgrouch@proton.me>
2022-07-04 18:23:01 -0400

    Adding first check in Grid#isValid

--- src/main/java/sudoku/puzzle/Grid.java
+++ src/main/java/sudoku/puzzle/Grid.java
@@ -1,5 +1,8 @@
 package sudoku.puzzle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Class representing a Sudoku grid with nine 3x3 regions of cells (a Nonet).
  * Can return the Nonets belonging to a particular row or column within this grid.
@@ -39,5 +42,38 @@ public class Grid {
 	 */
 	public Nonet[] getCol (int col) {
 		return nonets.getCol(col);
+	}
+
+	/**
+	 * Check the entire grid is valid (has only the numbers [1, 9] with no repeating digts) by:
+	 *
+	 * <p>1. Checking each Nonet is valid.</p>
+	 * <p>2. Checking every row is valid.</p>
+	 * <p>3. Checking every column is valid.</p>
+	 *
+	 * @return true if all three conditions are satisfied
+	 */
+	public boolean isValid () {
+		boolean validNonets = true;
+		boolean validRows = true;
+		boolean validCols = true;
+
+		// check all the Nonets are valid
+		for (int row = 0; row < rows; row++) {
+			Nonet[] nonetRow = getRow(row);
+			for (int col = 0; col < cols; col++) {
+				validNonets = nonetRow[col].isValid();
+				if (!validNonets) break;
+			}
+			if (!validNonets) break;
+		}
+
+		// check all the rows in the grid
+		ArrayList<Integer> rowNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+		// check all the columns in the grid
+		ArrayList<Integer> colNumbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+		return validNonets && validRows && validCols;
 	}
 }
