379001561bdac078c9994fc421de8c67b68f5f4c
Oscar <osgrouch@proton.me>
2022-07-05 16:10:09 -0400

    Adding getters for grid rows and columns

--- src/main/java/sudoku/puzzle/Grid.java
+++ src/main/java/sudoku/puzzle/Grid.java
@@ -6,34 +6,55 @@ package sudoku.puzzle;
  */
 public class Grid {
 	/** The number of Cell rows in this grid */
-	public static final int gridRows = 9;
+	public static final int rows = 9;
 	/** The number of Cell columns in this grid */
-	public static final int gridCols = 9;
-
-	/** The number of Cell rows in a region, also the number of regions in a column */
-	public static final int regionRows = 3;
-	/** The number of Cell columns in a region, also the number of regions in a row */
-	public static final int regionCols = 3;
+	public static final int cols = 9;
 
 	/** 2D Array of Cells in the puzzle */
 	private Cell[][] grid;
 
 	public Grid () {
-		this.grid = new Cell[gridRows][gridCols];
+		this.grid = new Cell[rows][cols];
 		int region = 1;
-		for (int row = 0; row < gridRows; row++) {
-			for (int col = 0; col < gridCols; col++) {
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
 				this.grid[row][col] = new Cell(region);
-				if (( col + 1 ) % regionCols == 0) {
-					// increment the region every time regionCols count has been reached
+				if (( col + 1 ) % 3 == 0) {
+					// increment the region every 3 columns
 					++region;
 				}
 			}
-			if (( ( row + 1 ) % regionRows ) != 0) {
-				// only allow region to be incremented every time regionRows count has been reached
-				// subtract by the number of columns in a region which is also the number of regions in a row
-				region -= regionCols;
+			if (( ( row + 1 ) % 3 ) != 0) {
+				// only allow region to be incremented every 3 rows
+				// subtract by the number of regions in a row: 3
+				region -= 3;
 			}
 		}
+	}
+
+	/**
+	 * Get the given row of Cells in the puzzle grid.
+	 * Used when verifying if current puzzle grid is a solution.
+	 *
+	 * @param row row number
+	 * @return Array of Cells
+	 */
+	private Cell[] getGridRow (int row) {
+		return grid[row];
+	}
+
+	/**
+	 * Get the given column of Cells in the puzzle grid.
+	 * Used when verifying if current puzzle grid is a solution.
+	 *
+	 * @param col column number
+	 * @return Array of Cells
+	 */
+	private Cell[] getGridCol (int col) {
+		Cell[] column = new Cell[rows];
+		for (int row = 0; row < rows; row++) {
+			column[row] = grid[row][col];
+		}
+		return column;
 	}
 }
