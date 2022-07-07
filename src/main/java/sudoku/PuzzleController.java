ff34071d61d17e59ad2a6d503428ead7433609bd
Oscar <osgrouch@proton.me>
2022-07-07 14:40:42 -0400

    Adding all sudoku cells and Controller class initialize method stubbing

--- src/main/java/sudoku/PuzzleController.java
+++ src/main/java/sudoku/PuzzleController.java
@@ -1,7 +1,33 @@
 package sudoku;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Controller for user interactions with Puzzle view in puzzle.fxml.
  */
-public class PuzzleController {
+public class PuzzleController implements Initializable {
+	@FXML
+	private GridPane board;
+
+	/** Default constructor. */
+	public PuzzleController () {}
+
+	/**
+	 * Called to initialize a controller after its root element has been completely processed.
+	 * Used to add display on hover property and toggle display on button press for each button
+	 * representing a pencil mark in each cell of the board.
+	 *
+	 * @param location  The location used to resolve relative paths for the root object, or
+	 *                  {@code null} if the location is not known.
+	 * @param resources The resources used to localize the root object, or {@code null} if
+	 *                  the root object was not localized.
+	 */
+	@Override
+	public void initialize (URL location, ResourceBundle resources) {
+	}
 }
