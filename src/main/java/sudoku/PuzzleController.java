8b7deaf6a0c1708550a6b2d400e6c8469877a902
Oscar <osgrouch@proton.me>
2022-07-07 18:58:15 -0400

    Adding undo and redo images

--- src/main/java/sudoku/PuzzleController.java
+++ src/main/java/sudoku/PuzzleController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
@@ -16,7 +18,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class PuzzleController implements Initializable {
 	/** The GridPane object that displays the different action buttons in the top section of the window */
 	@FXML
-	private GridPane actionPane;
+	private GridPane actionBar;
+	/** The undo button in the actionBar */
+	@FXML
+	private Button undoButton;
+	/** The redo button in the actionBar */
+	@FXML
+	private Button redoButton;
 
 	/** The GridPane object that displays the Sudoku puzzle */
 	@FXML
@@ -37,11 +45,23 @@ public class PuzzleController implements Initializable {
 	 */
 	@Override
 	public void initialize (URL location, ResourceBundle resources) {
-		// iterate through each button in the top actionPane to add background when hovered
-		for (Node node : actionPane.getChildren()) {
+		// iterate through each button in the top actionBar to add background when hovered
+		for (Node node : actionBar.getChildren()) {
 			node.setOnMouseEntered(event -> node.setStyle("-fx-background-color: #D5DCE0;"));
 			node.setOnMouseExited(event -> node.setStyle("-fx-background-color: transparent;"));
 		}
+
+		// set graphics for the undo and redo buttons
+		ImageView undoImg = new ImageView(new Image(getClass().getResourceAsStream("undo-arrow.png")));
+		undoImg.setFitHeight(20);
+		undoImg.setPreserveRatio(true);
+		undoImg.setOpacity(0.2);
+		ImageView redoImg = new ImageView(new Image(getClass().getResourceAsStream("redo-arrow.png")));
+		redoImg.setFitHeight(20);
+		redoImg.setPreserveRatio(true);
+		redoImg.setOpacity(0.2);
+		undoButton.setGraphic(undoImg);
+		redoButton.setGraphic(redoImg);
 
 		// iterate through each Sudoku annotation button to add display on hover and toggle display on press
 		for (Node node : board.getChildren()) {
