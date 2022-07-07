960b660cc3579794baee026ab23e1d814ffc05f9
Oscar <osgrouch@proton.me>
2022-07-07 18:23:59 -0400

    Adding background color to top buttons when hovered

--- src/main/java/sudoku/PuzzleController.java
+++ src/main/java/sudoku/PuzzleController.java
@@ -14,6 +14,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Controller for user interactions with Puzzle view in puzzle.fxml.
  */
 public class PuzzleController implements Initializable {
+	/** The GridPane object that displays the different action buttons in the top section of the window */
+	@FXML
+	private GridPane actionPane;
+
+	/** The GridPane object that displays the Sudoku puzzle */
 	@FXML
 	private GridPane board;
 
@@ -32,7 +37,13 @@ public class PuzzleController implements Initializable {
 	 */
 	@Override
 	public void initialize (URL location, ResourceBundle resources) {
-		// iterate through each button to add display on hover and toggle display on press
+		// iterate through each button in the top actionPane to add background when hovered
+		for (Node node : actionPane.getChildren()) {
+			node.setOnMouseEntered(event -> node.setStyle("-fx-background-color: #D5DCE0;"));
+			node.setOnMouseExited(event -> node.setStyle("-fx-background-color: transparent;"));
+		}
+
+		// iterate through each Sudoku annotation button to add display on hover and toggle display on press
 		for (Node node : board.getChildren()) {
 			for (Node child : ( (GridPane) node ).getChildren()) {
 				for (Node finalChild : ( (GridPane) child ).getChildren()) {
