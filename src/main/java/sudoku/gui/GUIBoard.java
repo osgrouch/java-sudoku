0f37ab971c87f7f18756f36cd7ef2cb021774fd7
Oscar <osgrouch@proton.me>
2022-07-09 19:28:00 -0400

    Completing init method to correctly link Cell classes and proper display

--- src/main/java/sudoku/gui/GUIBoard.java
+++ src/main/java/sudoku/gui/GUIBoard.java
@@ -1,10 +1,15 @@
 package sudoku.gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import sudoku.puzzle.SudokuBoard;
 
-public class GUIBoard {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GUIBoard implements Initializable {
 	/** The number of GUICells rows in this grid */
 	public static final int rows = 9;
 	/** The number of GUICells columns in this grid */
@@ -28,13 +33,51 @@ public class GUIBoard {
 		this.sudokuBoard = new SudokuBoard("input/sample_puzzle.csv");
 		// create a new GridPane
 		this.boardOfGUICells = new GUICell[rows][cols];
+	}
+
+	/**
+	 * Called to initialize a controller after its root element has been completely processed.
+	 * Populates the 2D Array of GUICells and links up each GUICell with its corresponding SudokuCell from the
+	 * SudokuBoard. Adds each GUICell Group to the GridPane to be displayed on the GUI.
+	 *
+	 * @param location  The location used to resolve relative paths for the root object, or
+	 *                  {@code null} if the location is not known.
+	 * @param resources The resources used to localize the root object, or {@code null} if
+	 *                  the root object was not localized.
+	 */
+	@Override
+	public void initialize (URL location, ResourceBundle resources) {
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
 				// link each GUICell with its corresponding SudokuCell in the SudokuBoard
 				GUICell current = new GUICell();
 				current.setSudokuCell(sudokuBoard.getSudokuCell(row, col));
-				this.boardOfGUICells[row][col] = current;
-				this.gridPaneOfGroups.getChildren().add(current.getGroup());
+				boardOfGUICells[row][col] = current;
+				gridPaneOfGroups.add(current.getGroup(), col, row);
+
+				// add slight margins to show visual distinction between sudoku regions
+				if (col == 2) {
+					GridPane.setMargin(current.getGroup(), new Insets(0, 4, 0, 0));
+				} else if (col == 6) {
+					GridPane.setMargin(current.getGroup(), new Insets(0, 0, 0, 4));
+				}
+				if (row == 2) {
+					if (col == 2) {
+						GridPane.setMargin(current.getGroup(), new Insets(0, 4, 4, 0));
+					} else if (col == 6) {
+						GridPane.setMargin(current.getGroup(), new Insets(0, 0, 4, 4));
+					} else {
+						GridPane.setMargin(current.getGroup(), new Insets(0, 0, 4, 0));
+					}
+				} else if (row == 6) {
+					if (col == 2) {
+						GridPane.setMargin(current.getGroup(), new Insets(4, 4, 0, 0));
+					} else if (col == 6) {
+						GridPane.setMargin(current.getGroup(), new Insets(4, 0, 0, 4));
+					} else {
+						GridPane.setMargin(current.getGroup(), new Insets(4, 0, 0, 0));
+					}
+				}
 			}
 		}
 	}
