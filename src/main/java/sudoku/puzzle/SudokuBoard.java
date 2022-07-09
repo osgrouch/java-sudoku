1860ca1daf3f19d8841b9ed2320bd1d8c1fadff9
Oscar <osgrouch@proton.me>
2022-07-09 18:39:30 -0400

    Changing structure of sudoku class

--- src/main/java/sudoku/puzzle/SudokuBoard.java
+++ src/main/java/sudoku/puzzle/SudokuBoard.java
@@ -10,22 +10,22 @@ import java.util.List;
  * Class representing a 9x9 Sudoku grid.
  * Keeps track of the Cells within the 9x9 grid.
  */
-public class Grid {
-	/** The number of Cell rows in this grid */
+public class SudokuGrid {
+	/** The number of SudokuCell rows in this grid */
 	public static final int rows = 9;
-	/** The number of Cell columns in this grid */
+	/** The number of SudokuCell columns in this grid */
 	public static final int cols = 9;
 
 	/** 2D Array of Cells in the puzzle */
-	private final Cell[][] grid;
+	private final SudokuCell[][] grid;
 
 	/**
-	 * Create a new Grid instance and populate the 2D Array of Cells with the values
+	 * Create a new SudokuGrid instance and populate the 2D Array of Cells with the values
 	 * from the given file. Does not check the file given is a file containing a Sudoku puzzle.
 	 *
 	 * @param filename a csv file containing a 9x9 Sudoku puzzle
 	 */
-	public Grid (String filename) {
+	public SudokuGrid (String filename) {
 		int[][] cellValues = new int[rows][cols];
 		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
 			String line;
@@ -41,11 +41,11 @@ public class Grid {
 			throw new RuntimeException(e);
 		}
 
-		this.grid = new Cell[rows][cols];
+		this.grid = new SudokuCell[rows][cols];
 		int region = 1;
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				this.grid[row][col] = new Cell(region, cellValues[row][col]);
+				this.grid[row][col] = new SudokuCell(region, cellValues[row][col]);
 				if (( col + 1 ) % 3 == 0) {
 					// increment the region every 3 columns
 					++region;
@@ -66,17 +66,17 @@ public class Grid {
 	 * @param region region to search for
 	 * @return ArrayList of Cells in the given region
 	 */
-	private Cell[] getRegion (int region) {
-		ArrayList<Cell> cells = new ArrayList<>(9); // there will only ever be 9 Cells in a region
+	private SudokuCell[] getRegion (int region) {
+		ArrayList<SudokuCell> sudokuCells = new ArrayList<>(9); // there will only ever be 9 Cells in a region
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
-				Cell current = grid[row][col];
+				SudokuCell current = grid[row][col];
 				if (current.getRegion() == region) {
-					cells.add(current);
+					sudokuCells.add(current);
 				}
 			}
 		}
-		return (Cell[]) cells.toArray();
+		return (SudokuCell[]) sudokuCells.toArray();
 	}
 
 	/**
@@ -86,7 +86,7 @@ public class Grid {
 	 * @param row row number
 	 * @return Array of Cells
 	 */
-	private Cell[] getGridRow (int row) {
+	private SudokuCell[] getGridRow (int row) {
 		return grid[row];
 	}
 
@@ -97,8 +97,8 @@ public class Grid {
 	 * @param col column number
 	 * @return Array of Cells
 	 */
-	private Cell[] getGridCol (int col) {
-		Cell[] column = new Cell[rows];
+	private SudokuCell[] getGridCol (int col) {
+		SudokuCell[] column = new SudokuCell[rows];
 		for (int row = 0; row < rows; row++) {
 			column[row] = grid[row][col];
 		}
@@ -106,21 +106,21 @@ public class Grid {
 	}
 
 	/**
-	 * Get the 2D Array of Cells stored within this Grid instance as an ArrayList of Cells
+	 * Get the 2D Array of Cells stored within this SudokuGrid instance as an ArrayList of Cells
 	 * from left to right per row, starting at row 0 and ending at row 8.
 	 *
 	 * @return ArrayList of Cells
 	 */
-	public ArrayList<Cell> getGridAsArrayList () {
-		ArrayList<Cell> cells = new ArrayList<>(81);
+	public ArrayList<SudokuCell> getGridAsArrayList () {
+		ArrayList<SudokuCell> sudokuCells = new ArrayList<>(81);
 		for (int row = 0; row < rows; row++) {
-			cells.addAll(List.of(getGridRow(row)));
+			sudokuCells.addAll(List.of(getGridRow(row)));
 		}
-		return cells;
+		return sudokuCells;
 	}
 
 	/**
-	 * Check if this Grid is a solution to the Sudoku puzzle by checking the following conditions:
+	 * Check if this SudokuGrid is a solution to the Sudoku puzzle by checking the following conditions:
 	 *
 	 * <ol>
 	 *     <li>Each region has no repeating numbers from [1, 9].</li>
@@ -128,7 +128,7 @@ public class Grid {
 	 *     <li>Each column has no repeating numbers from [1, 9].</li>
 	 * </ol>
 	 *
-	 * @return true if this Grid is a solution
+	 * @return true if this SudokuGrid is a solution
 	 */
 	public boolean isSolution () {
 		// used to create arraylists below to check for repeating numbers
@@ -136,42 +136,42 @@ public class Grid {
 
 		for (int region = 0; region < 9; region++) {
 			// there will only be 9 regions in a 9x9 Sudoku puzzle
-			Cell[] regionOfCells = getRegion(region);
+			SudokuCell[] regionOfSudokuCells = getRegion(region);
 			ArrayList<Integer> numbers = new ArrayList<>(listOfNumbers);
 
-			for (Cell cell : regionOfCells) {
-				Integer num = cell.getNumber();
+			for (SudokuCell sudokuCell : regionOfSudokuCells) {
+				Integer num = sudokuCell.getNumber();
 				if (!numbers.remove(num)) {
 					// remove returns false only if the given object was not in the ArrayList
-					// therefore a number was repeated in the region and this Grid is not a solution
+					// therefore a number was repeated in the region and this SudokuGrid is not a solution
 					return false;
 				}
 			}
 		}
 
 		for (int row = 0; row < rows; row++) {
-			Cell[] rowOfCells = getGridRow(row);
+			SudokuCell[] rowOfSudokuCells = getGridRow(row);
 			ArrayList<Integer> numbers = new ArrayList<>(listOfNumbers);
 
-			for (Cell cell : rowOfCells) {
-				Integer num = cell.getNumber();
+			for (SudokuCell sudokuCell : rowOfSudokuCells) {
+				Integer num = sudokuCell.getNumber();
 				if (!numbers.remove(num)) {
 					// remove returns false only if the given object was not in the ArrayList
-					// therefore a number was repeated in the row and this Grid is not a solution
+					// therefore a number was repeated in the row and this SudokuGrid is not a solution
 					return false;
 				}
 			}
 		}
 
 		for (int col = 0; col < cols; col++) {
-			Cell[] colOfCells = getGridCol(col);
+			SudokuCell[] colOfSudokuCells = getGridCol(col);
 			ArrayList<Integer> numbers = new ArrayList<>(listOfNumbers);
 
-			for (Cell cell : colOfCells) {
-				Integer num = cell.getNumber();
+			for (SudokuCell sudokuCell : colOfSudokuCells) {
+				Integer num = sudokuCell.getNumber();
 				if (!numbers.remove(num)) {
 					// remove returns false only if the given object was not in the ArrayList
-					// therefore a number was repeated in the column and this Grid is not a solution
+					// therefore a number was repeated in the column and this SudokuGrid is not a solution
 					return false;
 				}
 			}
@@ -181,7 +181,7 @@ public class Grid {
 	}
 
 	/**
-	 * Create a String representation of the current state of the Grid by displaying the Cell numbers
+	 * Create a String representation of the current state of the SudokuGrid by displaying the SudokuCell numbers
 	 * distinctly seperated by regions.
 	 *
 	 * @return visual String representation of 2D Array of Cells
